f3726ef119e44e6f11db6c40b8bfa3fd3c65bbf8
HyunJi Jo <joh244@newschool.edu>
2018-12-03 18:59:02 -0500

    ㅇ
    ㅇ

--- toy/week12.js
+++ toy/week12.js
@@ -30,19 +30,87 @@ box.style.animationPlayState = "paused"
 function wow(shark) {shark.src="eatshark.png";}
 function hul(shark) {shark.src="shark.png";}
 
-function changeimage() {
+
+function changeimage1() {
   var fish4 = document.querySelector("#fish4");
   fish4.src="dddfish.png"
-
-  var fish5 = document.querySelector("#fish5");
-  fish5.src="dddfish.png"
-
-  var fish6 = document.querySelector("#fish6");
-  fish6.src="dddfish.png"
-
-  var fish7 = document.querySelector("#fish7");
-  fish7.src="dddfish.png"
 }
+
+function changeimage2() {
+var fish5 = document.querySelector("#fish5");
+fish5.src="dddfish.png"
+}
+
+function changeimage3() {
+var fish6 = document.querySelector("#fish6");
+fish6.src="dddfish.png"
+}
+
+function changeimage4() {
+var fish7 = document.querySelector("#fish7");
+fish7.src="dddfish.png"
+}
+
+function changeimage5() {
+var fish8 = document.querySelector("#fish8");
+fish8.src="dddfish.png"
+}
+
+function changeimage6() {
+var fish9 = document.querySelector("#fish9");
+fish9.src="dddfish.png"
+}
+
+function changeimage7() {
+var fish10 = document.querySelector("#fish10");
+fish10.src="dddfish.png"
+}
+
+function changeimage8() {
+var fish11 = document.querySelector("#fish11");
+fish11.src="dddfish.png"
+}
+
+function changeimage9() {
+var fish12 = document.querySelector("#fish12");
+fish12.src="dddfish.png"
+}
+
+function changeimage10() {
+var fish13 = document.querySelector("#fish13");
+fish13.src="dddfish.png"
+}
+
+function changeimage11() {
+var fish14 = document.querySelector("#fish14");
+fish14.src="dddfish.png"
+}
+
+function changeimage12() {
+var fish15 = document.querySelector("#fish15");
+fish15.src="dddfish.png"
+}
+
+function changeimage13() {
+var fish16 = document.querySelector("#fish16");
+fish16.src="dddfish.png"
+}
+
+function changeimage14() {
+var fish17 = document.querySelector("#fish17");
+fish17.src="dddfish.png"
+}
+
+function changeimage15() {
+var fish18 = document.querySelector("#fish18");
+fish18.src="dddfish.png"
+}
+
+function changeimage16() {
+var fish19 = document.querySelector("#fish19");
+fish19.src="dddfish.png"
+}
+
 
 //뼈로 한번에 안바뀌게
 
@@ -66,5 +134,7 @@ function changeimage() {
 //var fishes = document.querySelectorAll("#fishes1, #fishes2, #fishes3, #btn")
 //fishes.addEventListener('onclick', function() {
 
-console.log(fishes)
-console.log(button)
+console.log(fish4)
+console.log(fish5)
+console.log(fish6)
+console.log(fish7)
