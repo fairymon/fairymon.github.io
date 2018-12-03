1ae3e088306a4a90d91b4d279f26ce6ed8834368
HyunJi Jo <joh244@newschool.edu>
2018-12-03 17:54:59 -0500

    d
    d

---
+++ mousetest.js
@@ -0,0 +1,5 @@
+
+
+function changeimage() {
+var image = document.querySelector(".fish");
+image.src="dddfish.png"}
--- toy/week12.js
+++ toy/week12.js
@@ -3,43 +3,68 @@ window.addEventListener(
 )
 
 var box = document.querySelector("#box")
+var shark = document.querySelector("shark.png")
 
 function onMouseMove (evt){
-console.log(evt.x+" "+evt.y)
-
 box.style.left = evt.x + "px"
 box.style.top = evt.y + "px"
-
 }
 
+
+
 var recognizer = new webkitSpeechRecognition()
-
-console.log(recognizer)
-
-
-
-
 
 document.addEventListener("mousemove", handleBox)
 
 function handleBox(evt) {
 var tag = evt.target.tagName.toLowerCase()
 
-if (tag == "a" || tag == "img") {
+if (tag == "img") {
 box.style.animationPlayState = "running"
 }
 else {
 box.style.animationPlayState = "paused"
 }
-
 }
 
+function wow(shark) {shark.src="eatshark.png";}
+function hul(shark) {shark.src="shark.png";}
 
-var fishes = document.querySelector("#fishes")
-fishes.addEventListener('click', function() {
-  if (fishes.getAttribute('click')) {
-    img.style.opacity = "0"
-  } else {
-    img.style.opacity = "1"
-  }
-}, false);
+function changeimage() {
+  var fish4 = document.querySelector("#fish4");
+  fish4.src="dddfish.png"
+
+  var fish5 = document.querySelector("#fish5");
+  fish5.src="dddfish.png"
+
+  var fish6 = document.querySelector("#fish6");
+  fish6.src="dddfish.png"
+
+  var fish7 = document.querySelector("#fish7");
+  fish7.src="dddfish.png"
+}
+
+//뼈로 한번에 안바뀌게
+
+//한번에 짧게 적는법
+
+
+//이거를 마우스위에 상어에다가 쓰고싶은데,,상어가 클릭할때 입을 벌렸으면 좋겠다
+
+//js
+//function wow(shark) {shark.src="eatshark.png";}
+//function hul(shark) {shark.src="shark.png";}
+//html
+//<div class="fishes1">
+//  <img class="bbshark" onmouseover="wow(this)" onmouseout="hul(this)" border="0"
+//   src="fish.png">
+//  <button class="btn" data-text-swap="crinkle"></button>
+//</div>
+//
+
+
+//var fishes = document.querySelectorAll("#fishes1, #fishes2, #fishes3, #btn")
+//fishes.addEventListener('onclick', function() {
+
+console.log(fishes)
+console.log(button)
